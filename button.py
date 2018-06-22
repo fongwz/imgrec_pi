865c83f2f4d33523fa16079032b87b0970cec451
fongwz <e0035221@u.nus.edu>
2018-06-22 15:37:57 +0800

    button interrupts

--- button.py
+++ button.py
@@ -5,6 +5,8 @@ import RPi.GPIO as GPIO
 import time
 import threading
 
+flag = 0;
+
 def my_callback(channel):
 	flag=1
 	print "button pressed"
@@ -12,7 +14,6 @@ def my_callback(channel):
 ##########setup############
 GPIO.setmode(GPIO.BCM)     # set up BCM GPIO numbering  
 GPIO.setup(25, GPIO.IN)    # set GPIO 25 as input
-flag = 0;
 GPIO.add_event_detect(25, GPIO.RISING, callback=my_callback, bouncetime=200)
 ##########################3
 
