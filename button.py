d9aff4cf690f26e94899f3ac5d95c69bf651fb63
fongwz <e0035221@u.nus.edu>
2018-06-22 16:22:15 +0800

    button interrupts

--- button.py
+++ button.py
@@ -8,7 +8,11 @@ import json
 from picamera import PiCamera
 from time import sleep
 
+
+################global variables#################
 flag = 0;
+#################################################
+
 
 def my_callback(channel):
 	global flag
@@ -45,21 +49,67 @@ def check_quality():
 	try:
 		if not data["faces"]:
 			print("no faces found")
+			return
 		else :
-			print
+			print "quality check passed"
+			return b64
 	except KeyError as err:
 		print("something went wrong..:%s" % format(err))
+		return
+
+def compare_img(b64):
+	#get faceset token first
+	print "getting faceset token..."
+	url="https://api-us.faceplusplus.com/facepp/v3/faceset/getfacesets"
+	payload={
+			'api_key':"_eLSJf561NiuuGdVKpahOF8soZpl7213"
+			,'api_secret':"lQ6frS9V67fLRE1mJjskziK7pyoJC2gN"   
+			}
+	response = requests.post(url, data=payload)
+	print(response.status_code, response.reason)
+	data = json.loads(response.text)
+	print("Faceset token is: %s" % data["facesets"][0]["faceset_token"])
+	faceset_token = data["facesets"][0]["faceset_token"]
+	print "---------------------------------------------"
+	sleep(1) #prevent qps error
+
+	print "now comparing..."
+	url="https://api-us.faceplusplus.com/facepp/v3/search"
+	payload={
+			'api_key':"_eLSJf561NiuuGdVKpahOF8soZpl7213"
+			,'api_secret':"lQ6frS9V67fLRE1mJjskziK7pyoJC2gN"   
+			,'image_base64': b64
+			,'faceset_token': faceset_token
+			,'return_result_count': 5	
+			}
+	response = requests.post(url, data=payload)
+	print(response.status_code, response.reason)
+	data = json.loads(response.text)
+	print(data)
+	print "---------------------------------------------"
+	if data["results"][0]["confidence"] > 87.0:
+		print("Matched face with user_id: %s at confidence level: %f" % (data["results"][0]["user_id"], data["results"][0]["confidence"]))
 
 ##########setup############
 GPIO.setmode(GPIO.BCM)     # set up BCM GPIO numbering  
 GPIO.setup(25, GPIO.IN)    # set GPIO 25 as input
 GPIO.add_event_detect(25, GPIO.RISING, callback=my_callback, bouncetime=200)
 camera = PiCamera()
-##########################3
+###########################
 
 
+#########main loop#########
 while(1):
 	if flag == 1:
 		print "handled event"
-		check_quality()
+		try:
+			b64 = check_quality()
+			sleep(1) #prevent qps error
+			compare_img(b64)
+			sleep(1) #prevent qps error
+		except TypeError as err:
+			print("Did not pass quality check: {0}".format(err))
+		
 		flag = 0
+		print "cleared flag"
+###########################
